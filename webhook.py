3b468c4d5dc7e9d4d9562857ffdb91dbb5a0bc5a
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-01-15 21:55:01 +0300

    upload files

--- webhook.py
+++ webhook.py
@@ -3,19 +3,14 @@ import requests
 WEBHOOK_URL = "https://discord.com/api/webhooks/1196524879739113512/pc9SjtfrXU0_82YfITqAXNEWuvEqokowgWueEqbRY_JsGJl2cuIwfstYJKltwnlHS7c8"
 
 def send_webhook_message(message):
-    # Mesaj boş mu diye kontrol et
     if not message.strip():
         print("Hata: Mesaj boş olamaz.")
         return
-
-    # JSON formatında veri gönder
     payload = {"content": message}
 
     try:
         response = requests.post(WEBHOOK_URL, json=payload)
-
-        # Yanıt kodunu kontrol et
-        if response.status_code == 204:  # HTTP 204, içerik gönderilmediğini belirtir
+        if response.status_code == 204:
             print("Mesaj başarıyla gönderildi.")
         else:
             print("Mesaj gönderme hatası. Yanıt Kodu:", response.status_code)
